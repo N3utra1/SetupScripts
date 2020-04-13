0e250d339abdd43ed43c6d79ce8f8838739e8bc1
William Riddell <Lambda@DESKTOP-60GG2DK.lan>
2020-04-13 03:45:48 +0100

    deleting old files

--- setupFileSystem.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#This script will set up the file system of the machine
-cd ~/Documents
-mkdir Projects
-cd ~/Pictures
-mkdir Wallpapers Memes
-cd ~/Pictures/Wallpapers
-mkdir Active
