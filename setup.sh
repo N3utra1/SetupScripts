da143e65d1dc068c832629ada1f4380c42cc7dea
William Riddell <Lambda@DESKTOP-60GG2DK.lan>
2020-05-01 04:35:25 +0100

    This is a commit adding in the config locations

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ cd ~/Pictures
 mkdir Wallpapers Memes
 cd ~/Pictures/Wallpapers
 mkdir Active
+cd
+mkdir .Bin
 
 #Now it will install the correct programs
 #In this part of the script I will be installing all the programs on the system, note this is for the dnf package manager
@@ -19,6 +21,8 @@ cd ~/Downloads
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 #This is a empty doom emacs install, I will leave it this way for now so each system wont get too bloated
-
+cd ~/Downloads
+mv config ~/.config/i3
+mv time.sh ~/.Bin
 #Now I need to set the wallpaper, point to a screenlayout location and sh file. I need to add the swap caps into the i3 config
 #Need to set all the keybinds in the i3 config but I will leave the doom emacs config untouched.
